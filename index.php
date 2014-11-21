bfdc3647288b871bf848aea332a5916e3e71eda7
nickyvandijk <nvdijk@ictmbo.nl>
2014-11-21 09:13:19 +0100

    dat is een mooie aanpassing
    index php toegevoegd

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Naamloos document</title>
+</head>
+
+<body>
+	<header></header>
+    <section><article><p>broodje kip</p></article></section>
+    <footer></footer>
+</body>
+</html>
